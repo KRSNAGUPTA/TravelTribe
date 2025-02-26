2913e22090de4a5f7330c92a727a23d8485a7218
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-26 23:48:37 +0530

    style: format paymentRoutes.js for improved readability and consistency

--- server/src/routes/paymentRoutes.js
+++ server/src/routes/paymentRoutes.js
@@ -1,12 +1,15 @@
-import Router from "express"
-import { createOrder, verifyPayment } from "../controllers/paymentController.js";
+import Router from "express";
+import {
+  createOrder,
+  verifyPayment,
+} from "../controllers/paymentController.js";
 
 const router = Router();
-router.get("/",(req,res)=>{
-    return res.status(200).json({
-        message:"Welcome to Payment Route"
-    })
-})
-router.post("/create-order",createOrder)
-router.post("/verify-payment",verifyPayment)
+router.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to Payment Route",
+  });
+});
+router.post("/create-order", createOrder);
+router.post("/verify-payment", verifyPayment);
 export default router;
