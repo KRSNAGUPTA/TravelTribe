199d078d738a46b5527ce3932d3a8ac358e8f816
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-02-27 20:31:17 +0530

    feat: implement contact form with validation and Discord notification for support requests

--- server/src/discordBot/NotificationBot.js
+++ server/src/discordBot/NotificationBot.js
@@ -1,14 +1,32 @@
 import axios from "axios";
 export const sendNotification = async (type, data) => {
   let message = "";
+  if (!process.env.DISCORD_BOT_WEBHOOK) return;
+  if (!type) {
+    console.error("No type provided for notification");
+    return;
+  }
   if (type === "login") {
-    message = `User ${data.name} logged in`;
+    message = `User ${
+      data.name
+    } logged in at ${new Date().toLocaleTimeString()} on ${new Date().toLocaleDateString()}`;
     await axios.post(process.env.DISCORD_BOT_WEBHOOK, {
       content: `${message}`,
     });
   }
   if (type === "register") {
-    message = `User ${data.name} logged out`;
+    message = `User ${
+      data.name
+    } register at ${new Date().toLocaleTimeString()} on ${new Date().toLocaleDateString()}`;
   }
-  console.log(message);
+  if (type === "support") {
+    message = `${data.name} with email ${data.email} on topic ${data.topic} \nsent a message: ${data.message}`;
+  }
+  if (!message) {
+    console.error(`Type ${type} not supported. Please check the type`);
+    return;
+  }
+  await axios.post(process.env.DISCORD_BOT_WEBHOOK, {
+    content: `${message}`,
+  });
 };
--- server/src/index.js
+++ server/src/index.js
@@ -5,14 +5,17 @@ import userRoutes from "./routes/userRoutes.js";
 import hostelRoutes from "./routes/hostelRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
-import cors from "cors"
+import cors from "cors";
+import { sendNotification } from "./discordBot/NotificationBot.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin:process.env.CORS_ORIGIN
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 const PORT = process.env.PORT;
 
 connectDB();
@@ -25,6 +28,20 @@ app.use("/api/user", userRoutes);
 app.use("/api/hostel", hostelRoutes);
 app.use("/api/booking", bookingRoutes);
 app.use("/api/payment", paymentRoutes);
+
+//for discord bot notification
+app.post("/api/support", async (req, res) => {
+  const { name, email, topic, message } = req.body;
+  await sendNotification("support", {
+    name,
+    email,
+    topic,
+    message,
+  });
+  res.status(200).json({
+    message: "Support request received",
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
